8738f45c1dd52bf5f50e93c8b47e4b1f4a969c7e
Imran <muhdimran1810@gmail.com>
2021-03-18 16:21:20 +0800

    Exercise 7

--- Lab1/src/drive.java
+++ Lab1/src/drive.java
@@ -1,6 +1,7 @@
-
+import java.util.Scanner;
 public class drive {
 	 public static void main(String args[]) {
-		 System.out.println("wth");
+		 weightcal wt = new weightcal(110);
+		 wt.weight();
 	 }
 }
---
+++ Lab1/src/weightcal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class weightcal {
+	
+	double mass;
+	
+	weightcal(double mass){
+		this.mass = mass;
+	}
+	
+	void setMass(double mass) {
+		this.mass = mass;
+	}
+	
+	public void weight() {
+		double earthWeight = mass * 9.80665;
+		double marsWeight = mass * 3.711;
+		
+		System.out.println("Mass: "+mass+"KG\nWeight on Earth: " +earthWeight+"N\nWeight on Mars: "+marsWeight+"N");
+	}
+}
